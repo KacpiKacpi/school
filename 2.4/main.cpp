73cb513b3f946909aa0370ae5974654af2ef424d
Kacper <egracz123@gmail.com>
2021-06-28 10:46:00 +0200

    Added 2.4 lesson(checking if triangular exists

---
+++ 2.4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"a= ";cin>>a;
+    cout<<"b= ";cin>>b;
+    cout<<"c= ";cin>>c;
+    if(a+b>c && a+c>b && b+c>a )cout<<"Tak";
+    else cout<<"NIE;
+    return 0;
+}
