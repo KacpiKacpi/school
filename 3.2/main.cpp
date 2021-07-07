e2253f376bf3c24f965b815ea25d957cbec8fa5d
Kacper <egracz123@gmail.com>
2021-07-07 16:51:18 +0200

    Added secodn way

--- 3.2/main.cpp
+++ 3.2/main.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main()
 {
-    int  dec=0 , p=1;
+    int  dec=0 , p=1,w;
     string b;
     cout << "Enter binary number: " ; cin>>b ; cout<<endl;
-    for(int i = b.size()-1 ; i>=0  ; i--)
+    cout<<"Which way do you prefer 1 or 2: "; cin>>w;
+    if(w==1)
+        for(int i = b.size()-1 ; i>=0  ; i--)
+        {
+            if(b[i]=='1')dec+=p;
+            p*=2;
+        }
+    else if(w==2)
+    for(int i = 0; i<b.size();i++)
     {
-        if(b[i]=='1')dec+=p;
-        p*=2;
+        dec*=2;
+        if(b[i]=='1')dec++;
     }
     cout<<dec;
     return 0;
